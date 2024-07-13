91dc7bc55de5d316a1f3252fdaa2def1725dec62
blackshibe <kacperszczepan@tutanota.com>
2024-07-13 16:44:05 +0200

    oops

--- luau/server/gamemode_setup.lua
+++ luau/server/gamemode_setup.lua
@@ -2,9 +2,9 @@
 
 -- this script loads the map configuration for every match
 
-local shooting_range_config = config.map.SHOOTING_RANGE_CONFIGURATION
-local map_config = config.map.MAP_CONFIGURATION
-local studio_config = config.map.STUDIO_CONFIGURATION
+local shooting_range_config = config.maps.SHOOTING_RANGE_CONFIGURATION
+local map_config = config.maps.MAP_CONFIGURATION
+local studio_config = config.maps.STUDIO_CONFIGURATION
 
 local function random_value_in_map(map)
 	local entries = {}
@@ -18,11 +18,11 @@ local function random_value_in_map(map)
 	return map[key]
 end
 
-if config.map.place_config == "Match" then
+if config.maps.place_config == "Match" then
 	print("Using shooting range map configuration")
-	config.map.set_map_from_config(shooting_range_config)
+	config.maps.set_map_from_config(shooting_range_config)
 else
-	config.map.set_map_from_config(is_studio() and studio_config() or random_value_in_map(map_config))
+	map.set_map_from_config(is_studio() and studio_config() or random_value_in_map(map_config))
 	gamemode.gamemode_finished:Connect(function(avoid_resetting_map)
 		if avoid_resetting_map then
 			return
@@ -33,7 +33,7 @@ else
 		sharedvars.sv_spawning_enabled = false
 		set_spawning_disabled_reason("Intermission between matches")
 
-		if SharedState.sv_map_voting.value then
+		if sharedvars.sv_map_voting then
 			local voted_map = map.run_vote()
 			map.set_map_from_config(map_config[voted_map])
 		else
